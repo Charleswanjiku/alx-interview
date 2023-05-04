0b0be3a9377e16f0b4de16e0a2d5db17721818bb
Charleswanjiku <kangethewanjiku@gmail.com>
2023-05-04 13:09:46 +0000

    min op

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 
-
-
 def minOperations(n):
-    
-    if not isinstance(n, int) or n <= 1:
-        return 0
 
-    factors = []
-    i = 2
-    while i <= n:
+    if n < 2:
+        return 0
+    factor_list = []
+    i = 1
+    while n != 1:
+        i += 1
         if n % i == 0:
-            factors.append(i)
-            n = n / i
-        else:
-            i += 1
-    return sum(factors)
+            while n % i == 0:
+                n /= i
+                factor_list.append(i)
+     return sum(factor_list)
